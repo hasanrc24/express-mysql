e7d5ec2c4363b2c7dafe2de0135454f6a225c78a
Md Hasan <hasanrc24@gmail.com>
2024-07-28 09:49:55 +0600

    Seeder updated

--- seeders/20240728032212-populate-uuid-in-users.js
+++ seeders/20240728032212-populate-uuid-in-users.js
@@ -5,19 +5,29 @@ const { v4: uuidv4 } = require('uuid');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const users = await queryInterface.sequelize.query(
-      'SELECT id FROM Users',
-      { type: Sequelize.QueryTypes.SELECT }
-    );
+    // const users = await queryInterface.sequelize.query(
+    //   'SELECT id FROM Users',
+    //   { type: Sequelize.QueryTypes.SELECT }
+    // );
+    const User = queryInterface.sequelize.define('User', {
+      uuid: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      }
+    });
+
+    const users = await User.findAll();
 
     for (const user of users) {
-      await queryInterface.sequelize.query(
-        'UPDATE Users SET uuid = :uuid WHERE id = :id',
-        {
-          replacements: { uuid: uuidv4(), id: user.id },
-          type: Sequelize.QueryTypes.UPDATE,
-        }
-      );
+      // await queryInterface.sequelize.query(
+      //   'UPDATE Users SET uuid = :uuid WHERE id = :id',
+      //   {
+      //     replacements: { uuid: uuidv4(), id: user.id },
+      //     type: Sequelize.QueryTypes.UPDATE,
+      //   }
+      // );
+      await user.update({ uuid: uuidv4() })
     }
     /**
      * Add seed commands here.
